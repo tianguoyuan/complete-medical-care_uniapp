10baeb374a3d96d63296eaa2ee2432138ac8a0a7
tianguoyuan <603660895@qq.com>
2026-05-19 17:37:34 +0800

    feat: ✨ 创建选择家庭成员页面

---
+++ src/pages/home/selectFamilyMembers.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import Navbar from '@/components/Navbar.vue'
+//
+</script>
+
+<template>
+  <view class="">
+    <Navbar fixed show-back-icon title="新建居民档案" />
+  </view>
+</template>
+
+<style lang="scss" scoped>
+//
+</style>
+
+<route lang="json5" type="page">
+{
+  layout: 'default',
+  style: {
+    navigationStyle: 'custom',
+    navigationBarTitleText: '选择家庭成员',
+  },
+}
+</route>
