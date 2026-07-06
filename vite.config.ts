e4fce3b4917e5851f66cce27afefe970e6bda25b
tianguoyuan <603660895@qq.com>
2026-07-06 16:38:30 +0800

    perf: 👌增加gzip压缩vite-plugin-compression

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ import { visualizer } from 'rollup-plugin-visualizer'
 import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig, loadEnv } from 'vite'
+import ViteCompression from 'vite-plugin-compression'
 import ViteRestart from 'vite-plugin-restart'
 
 import pkg from './package.json'
@@ -139,6 +140,8 @@ export default defineConfig(({ command, mode }) => {
           return html.replace('%BUILD_TIME%', dayjs().format('YYYY-MM-DD HH:mm:ss'))
         },
       },
+      // gzip 压缩，h5 + 生产环境
+      UNI_PLATFORM === 'h5' && mode === 'production' && ViteCompression({ algorithm: 'gzip' }),
       // 打包分析插件，h5 + 生产环境才弹出
       UNI_PLATFORM === 'h5' &&
         mode === 'production' &&
